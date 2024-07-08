20a75be234deff0b61859df8e151c0a63f46fff6
Ishwar Mishra <ikmishra2002@gmail.com>
2024-07-08 20:04:32 +0530

    create signup UI

--- frontend/src/pages/SignUp.jsx
+++ frontend/src/pages/SignUp.jsx
@@ -1,10 +1,62 @@
-import React from 'react'
+import { useState } from "react"
+import { Link } from "react-router-dom"
 
 const SignUp = () => {
+
+  const [formData, setFormData] = useState({
+    username: "",
+    password: "",
+    confirmPassword: "",
+    email: ""
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({
+      ...formData,
+      [name]: value
+    });
+  }
+
   return (
-    <div>
-      SignUp
-      SignUp
+    <div className='p-3 max-w-lg mx-auto'>
+      <h1 className='text-3xl text-center font-bold my-7'>Sign Up</h1>
+      <form className='flex flex-col gap-4'>
+        <input type="text"
+          className='w-full rounded-md p-2'
+          placeholder='Username'
+          name="username"
+          onChange={handleChange}
+          required />
+
+        <input type="email"
+          className='w-full rounded-md p-2'
+          placeholder='Email'
+          name="email"
+          onChange={handleChange}
+          required />
+
+        <input type="text"
+          className='w-full rounded-md p-2'
+          placeholder='Password'
+          name="password"
+          onChange={handleChange}
+          required />
+
+        <input type="text"
+          className='w-full rounded-md p-2'
+          placeholder='Confirm Password'
+          name="confirmPassword"
+          onChange={handleChange}
+          required />
+
+        <button type='submit' className='bg-slate-700 p-2 rounded-md text-lg text-white hover:bg-slate-800 disabled:opacity-80 transition duration-200'>Sign Up</button>
+      </form>
+      <div>
+        <Link>
+          <p className="text-blue-700 mt-4 hover:underline">Have an account?</p>
+        </Link>
+      </div>
     </div>
   )
 }
